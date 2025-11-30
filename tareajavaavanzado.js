c394f5831b79865e9d32e48ac072c24f46974cf4
Franco Tissera <frantissera18@gmail.com>
2025-11-29 22:03:27 -0300

    TareaJavaAvanzado.js

---
+++ tareajavaavanzado.js
@@ -0,0 +1,8 @@
+function calcularPromedio(nota1, nota2) {
+  let promedio = (nota1 + nota2) / 2;
+  return promedio;
+}
+
+let historia = calcularPromedio(7, 9);
+console.log("El promedio en Historia es: " + historia);
+
